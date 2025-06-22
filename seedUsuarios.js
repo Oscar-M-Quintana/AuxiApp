31ae029020a9ce4d91b952615b4bdbd143eff282
Oscar M. Quintana <oscarlely@gmail.com>
2025-06-21 22:26:12 -0300

    Agrego script para crear usuario operador

--- seedUsuarios.js
+++ seedUsuarios.js
@@ -1,49 +1,20 @@
+// seedUsuarios.js
 const bcrypt = require("bcrypt");
 const sequelize = require("./database");
 const Usuario = require("./models/Usuario");
 
 async function seed() {
-  await sequelize.sync({ force: true });
-
-  const hashedPassword = await bcrypt.hash("1234", 10);
-
-  await Usuario.bulkCreate([
-    {
-      email: "operador1@lacaja.com.ar",
-      password: hashedPassword,
-      rol: "operador"
-    },
-    {
-      email: "operador2@atm.com.ar",
-      password: hashedPassword,
-      rol: "operador"
-    },
-    {
-      email: "aseguradora1@lacaja.com.ar",
-      password: hashedPassword,
-      rol: "aseguradora"
-    },
-    {
-      email: "aseguradora2@atm.com.ar",
-      password: hashedPassword,
-      rol: "aseguradora"
-    },
-    {
-      email: "grua1@auxilios.com",
-      password: hashedPassword,
-      rol: "auxilio",
-      tipo_auxilio: "grúa"
-    },
-    {
-      email: "mecanico1@auxilios.com",
-      password: hashedPassword,
-      rol: "auxilio",
-      tipo_auxilio: "mecánica ligera"
-    }
-  ]);
-
-  console.log("Usuarios creados con éxito");
+  await sequelize.sync({ force: false });
+  const hash = await bcrypt.hash("123456", 10);
+  await Usuario.create({
+    nombre: "Operador Prueba",
+    email: "operador@auxiapp.com",
+    password: hash,
+    rol: "operador"
+  });
+  console.log("Operador creado");
   process.exit();
 }
 
-seed();
+seed().catch(console.error);
+
